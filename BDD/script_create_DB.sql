b76019c9ad42081c83ce50ceefa8d780eef29b5e
letronul <moi@email.com>
2016-02-12 16:54:13 +0100

    first commit

---
+++ BDD/script_create_DB.sql
@@ -0,0 +1,321 @@
+drop database if exists projet_bd;
+create database projet_bd;
+use projet_bd;
+
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: User
+#------------------------------------------------------------
+
+CREATE TABLE User(
+        id_user      Int NOT NULL ,drop database if exists projet_bd;
+create database projet_bd;
+use projet_bd;
+
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: User
+#------------------------------------------------------------
+
+CREATE TABLE User(
+        id_user      Int NOT NULL ,
+        login        Varchar (128) NOT NULL,
+        password     Varchar (128) NOT NULL ,
+        nom          Varchar (64) ,
+        prenom       Varchar (64) ,
+        adresse      Varchar (128) ,
+        code_postale Varchar (128) ,
+        telephone    Varchar (128) ,
+        mail         Varchar (128) ,
+        privilege    Varchar (128) ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Professeur
+#------------------------------------------------------------
+
+CREATE TABLE Professeur(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: rf
+#------------------------------------------------------------
+
+CREATE TABLE rf(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: admin
+#------------------------------------------------------------
+
+CREATE TABLE admin(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Matiere
+#------------------------------------------------------------
+
+CREATE TABLE Matiere(
+        id_matiere   Int NOT NULL ,
+        ref          Varchar (25) ,
+        libelle      Varchar (25) ,
+        coefficient  Int ,
+        credtit_ects Int ,
+        id_user      Int ,
+        id_formation Int ,
+        PRIMARY KEY (id_matiere )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Classe
+#------------------------------------------------------------
+
+CREATE TABLE Classe(
+        id_classe    Int NOT NULL ,
+        libelle      Varchar (128) ,
+        annee        Varchar (25) ,
+        id_formation Int ,
+        PRIMARY KEY (id_classe )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: eleve
+#------------------------------------------------------------
+
+CREATE TABLE eleve(
+        id_user   Int NOT NULL ,
+        nom       Varchar (64) NOT NULL ,
+        prenom    Varchar (64) NOT NULL ,
+        adresse   Varchar (128) NOT NULL ,
+        ville     Varchar (128) NOT NULL ,
+        telephone Varchar (128) NOT NULL ,
+        mail      Varchar (128) ,
+        id_classe Int ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: note
+#------------------------------------------------------------
+
+CREATE TABLE note(
+        id_note     Int NOT NULL ,
+        libelle     Varchar (128) ,
+        barem       Int ,
+        coefficient Int ,
+        date_note   Varchar (25) ,
+        id_matiere  Int ,
+        PRIMARY KEY (id_note )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Formation
+#------------------------------------------------------------
+
+CREATE TABLE Formation(
+        id_formation Int NOT NULL ,
+        libelle      Varchar (128) ,
+        id_user      Int ,
+        PRIMARY KEY (id_formation )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Posseder
+#------------------------------------------------------------
+
+CREATE TABLE Posseder(
+        note    Int ,
+        id_user Int NOT NULL ,
+        id_note Int NOT NULL ,
+        PRIMARY KEY (id_user ,id_note )
+)ENGINE=InnoDB;
+
+ALTER TABLE Professeur ADD CONSTRAINT FK_Professeur_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE rf ADD CONSTRAINT FK_rf_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE admin ADD CONSTRAINT FK_admin_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Matiere ADD CONSTRAINT FK_Matiere_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Matiere ADD CONSTRAINT FK_Matiere_id_formation FOREIGN KEY (id_formation) REFERENCES Formation(id_formation);
+ALTER TABLE Classe ADD CONSTRAINT FK_Classe_id_formation FOREIGN KEY (id_formation) REFERENCES Formation(id_formation);
+ALTER TABLE eleve ADD CONSTRAINT FK_eleve_id_classe FOREIGN KEY (id_classe) REFERENCES Classe(id_classe);
+ALTER TABLE note ADD CONSTRAINT FK_note_id_matiere FOREIGN KEY (id_matiere) REFERENCES Matiere(id_matiere);
+ALTER TABLE Formation ADD CONSTRAINT FK_Formation_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Posseder ADD CONSTRAINT FK_Posseder_id_user FOREIGN KEY (id_user) REFERENCES eleve(id_user);
+ALTER TABLE Posseder ADD CONSTRAINT FK_Posseder_id_note FOREIGN KEY (id_note) REFERENCES note(id_note);
+
+
+alter table User modify id_user Int auto_increment;
+alter table Matiere modify id_matiere Int auto_increment;
+alter table Classe modify id_classe Int auto_increment;
+alter table eleve modify id_user Int auto_increment;
+alter table note modify id_note Int auto_increment;
+alter table Formation modify id_formation Int auto_increment;
+        login        Varchar (128) NOT NULL,
+        password     Varchar (128) NOT NULL ,
+        nom          Varchar (64) ,
+        prenom       Varchar (64) ,
+        adresse      Varchar (128) ,
+        code_postale Varchar (128) ,
+        telephone    Varchar (128) ,
+        mail         Varchar (128) ,
+        privilege    Varchar (128) ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Professeur
+#------------------------------------------------------------
+
+CREATE TABLE Professeur(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: rf
+#------------------------------------------------------------
+
+CREATE TABLE rf(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: admin
+#------------------------------------------------------------
+
+CREATE TABLE admin(
+        id_user Int NOT NULL ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Matiere
+#------------------------------------------------------------
+
+CREATE TABLE Matiere(
+        id_matiere   Int NOT NULL ,
+        ref          Varchar (25) ,
+        libelle      Varchar (25) ,
+        coefficient  Int ,
+        credtit_ects Int ,
+        id_user      Int ,
+        id_formation Int ,
+        PRIMARY KEY (id_matiere )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Classe
+#------------------------------------------------------------
+
+CREATE TABLE Classe(
+        id_classe    Int NOT NULL ,
+        libelle      Varchar (128) ,
+        annee        Varchar (25) ,
+        id_formation Int ,
+        PRIMARY KEY (id_classe )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: eleve
+#------------------------------------------------------------
+
+CREATE TABLE eleve(
+        id_user   Int NOT NULL ,
+        nom       Varchar (64) NOT NULL ,
+        prenom    Varchar (64) NOT NULL ,
+        adresse   Varchar (128) NOT NULL ,
+        ville     Varchar (128) NOT NULL ,
+        telephone Varchar (128) NOT NULL ,
+        mail      Varchar (128) ,
+        id_classe Int ,
+        PRIMARY KEY (id_user )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: note
+#------------------------------------------------------------
+
+CREATE TABLE note(
+        id_note     Int NOT NULL ,
+        libelle     Varchar (128) ,
+        barem       Int ,
+        coefficient Int ,
+        date_note   Varchar (25) ,
+        id_matiere  Int ,
+        PRIMARY KEY (id_note )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Formation
+#------------------------------------------------------------
+
+CREATE TABLE Formation(
+        id_formation Int NOT NULL ,
+        libelle      Varchar (128) ,
+        id_user      Int ,
+        PRIMARY KEY (id_formation )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Posseder
+#------------------------------------------------------------
+
+CREATE TABLE Posseder(
+        note    Int ,
+        id_user Int NOT NULL ,
+        id_note Int NOT NULL ,
+        PRIMARY KEY (id_user ,id_note )
+)ENGINE=InnoDB;
+
+ALTER TABLE Professeur ADD CONSTRAINT FK_Professeur_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE rf ADD CONSTRAINT FK_rf_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE admin ADD CONSTRAINT FK_admin_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Matiere ADD CONSTRAINT FK_Matiere_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Matiere ADD CONSTRAINT FK_Matiere_id_formation FOREIGN KEY (id_formation) REFERENCES Formation(id_formation);
+ALTER TABLE Classe ADD CONSTRAINT FK_Classe_id_formation FOREIGN KEY (id_formation) REFERENCES Formation(id_formation);
+ALTER TABLE eleve ADD CONSTRAINT FK_eleve_id_classe FOREIGN KEY (id_classe) REFERENCES Classe(id_classe);
+ALTER TABLE note ADD CONSTRAINT FK_note_id_matiere FOREIGN KEY (id_matiere) REFERENCES Matiere(id_matiere);
+ALTER TABLE Formation ADD CONSTRAINT FK_Formation_id_user FOREIGN KEY (id_user) REFERENCES User(id_user);
+ALTER TABLE Posseder ADD CONSTRAINT FK_Posseder_id_user FOREIGN KEY (id_user) REFERENCES eleve(id_user);
+ALTER TABLE Posseder ADD CONSTRAINT FK_Posseder_id_note FOREIGN KEY (id_note) REFERENCES note(id_note);
+
+
+alter table User modify id_user Int auto_increment;
+alter table Matiere modify id_matiere Int auto_increment;
+alter table Classe modify id_classe Int auto_increment;
+alter table eleve modify id_user Int auto_increment;
+alter table note modify id_note Int auto_increment;
+alter table Formation modify id_formation Int auto_increment;
---
+++ connexion.php
@@ -0,0 +1,52 @@
+<!-- PRECODE ICI -->
+
+
+<!DOCTYPE html>
+<html lang="fr">
+	
+	<?php include("include/header.html"); ?>
+
+	<body>
+		
+		<h1>APPLI SWAG PROF</h1>
+
+		<div class="content">
+			
+			<div class="connexion">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						
+							<!-- CODE ICI -->
+							
+							
+							
+							<form method="post" action="index.php">
+							<table class="connexion">
+								<tr>
+									<td><h2><b>Connexion</b></h2></td>
+								</tr>
+								<tr>
+									<td><b>Login : </b></td> <td><input type="text" name="pseudo" ></code> </td>
+								</tr>
+								<tr>
+									<td><b>Mots de passe :  </b></td> <td><input type="password" name="password" ></code> </td>
+								</tr>
+								<tr style="text-align: center;">
+									<td colspan="2"><input type="submit" value="Valider" ></code></td>
+								</tr>
+							</table>	
+							</form>
+						
+					</div>
+				</div>		
+			</div>
+			
+			<?php include("include/footer.php"); ?>
+			
+		</div>
+	</body>
+	
+	<?php include("include/script.html"); ?>
+</html>
+ 
+ 
