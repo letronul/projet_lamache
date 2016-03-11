8b92d69d442a6ef34a949fc7fa7f5e58680c3544
letronul <moi@email.com>
2016-03-11 09:42:01 +0100

    clean git

--- include/footer.php
+++
@@ -1,8 +0,0 @@
-			<div class="footer">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<img src="/proj_lamache/images/logo.png"><br>
-						<p>copyRight Remy corp  www.remy-le-bg@beaugoss.com</p>
-					</div>
-				</div>
-			</div>
